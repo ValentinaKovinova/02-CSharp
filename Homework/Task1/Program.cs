188d649d83d6f0ceba9ee0a65ee2f3ed827057d2
Valentina_Kovinova <vkovinova@gmail.com>
2024-04-25 08:44:37 +0800

    Added an algorithm for solving the Task1

--- Homework/Task1/Program.cs
+++ Homework/Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int remainDiv7 = num % 7;
+int remainDiv23 = num % 23;
+
+if (remainDiv7 == 0 && remainDiv23 == 0)
+{
+    Console.WriteLine($"Число {num} кратно одновременно числам 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {num} не кратно одновременно числам 7 и 23");
+}
