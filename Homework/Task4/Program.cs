8c7815ccfc06eeee540f20231bf31ab0c0f4cdb6
Valentina_Kovinova <vkovinova@gmail.com>
2024-05-12 18:48:29 +0800

    Added an algorithm for solving the Task4

---
+++ Homework/Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, 
+//а на выходе показывает его цифры через запятую.
+
+
+Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num <= 0)
+{
+    Console.WriteLine($"Некорректный ввод");
+    return;
+}
+else
+{
+    while (num > 0)
+    {
+        int remain = num / 10;
+        if (remain > 0)
+        { Console.Write($"{num % 10}, "); }
+        else
+        { Console.Write(num % 10); }
+        num /= 10;
+    }
+}
