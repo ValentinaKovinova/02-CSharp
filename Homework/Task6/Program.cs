e7c17a7722a222a9beb9a403e0194ece1236a596
Valentina_Kovinova <vkovinova@gmail.com>
2024-06-02 15:59:58 +0800

    Added an algorithm for solving the Task6

---
+++ Homework/Task6/Program.cs
@@ -0,0 +1,23 @@
+﻿//Найдите произведения пар чисел в одномерном массиве. Парой
+//считаем первый и последний элемент, второй и предпоследний
+//и т.д. Результат запишем в новый массив.
+
+//Пример:
+//[1, 3, 2, 4, 2, 3] => [3 6 8]
+//[2, 3, 1, 7, 5, 6, 3] => [6 18 5] (элемент 7 не имеет пары)
+
+int[] array = new int[] {1, 3, 2, 4, 2, 3};
+int[] arrayResult = new int[array.Length / 2];
+
+for (int i = 0; i < arrayResult.Length; i++)
+{
+    arrayResult[i] = array[i] * array[array.Length - 1 - i];
+}
+for (int i = 0; i < arrayResult.Length; i++)
+{
+    Console.Write(arrayResult[i] + " ");
+}
+if (array.Length % 2 != 0)
+{
+   Console.Write("(элемент " + array[arrayResult.Length] + " не имеет пары)"); 
+}
